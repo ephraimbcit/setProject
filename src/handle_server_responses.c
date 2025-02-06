7c2a5964b23fc22f6e2581f3b52c25af864324b9
Ephraim Hsu <ehsu18@my.bcit.ca>
2025-02-06 11:28:43 -0800

    SAVE work

--- src/handle_server_responses.c
+++ src/handle_server_responses.c
@@ -3,55 +3,50 @@
 //
 
 #include "../include/handle_server_responses.h"
+#include <stdint.h>
 #include <unistd.h>
 
-
 #define RESPONSE_HEADER_SIZE 4
-#define SERVER_MANAGER_REQUEST_MAX_SIZE 2
 #define REQUIRED_PROTOCOL_VERSION 0x01
-#define UTF8STRING_PROTOCOL 0x0C
-#define USER_COUNT 0x0A
-#define ACTIVE_USER_COUNT 0x0B
+// #define UTF8STRING_PROTOCOL 0x0C
+// #define USER_COUNT 0x0A
+// #define ACTIVE_USER_COUNT 0x0B
 #define RESPONSE_TYPE_INDEX 0
 #define RESPONSE_VERSION_INDEX 1
-#define RESPONSE_PAYLOAD_LENGTH_INDEX 3
+#define RESPONSE_PAYLOAD_LENGTH_INDEX_1 2
+#define RESPONSE_PAYLOAD_LENGTH_INDEX_2 3
+#define BIT_SHIFT_BIG_ENDIAN 8
 
-void parse_response(int type, void *server_fd_ptr, int payload_length)
-{
-    int server_fd;
-    unsigned char response[payload_length + RESPONSE_HEADER_SIZE];
-    unsigned char payload[payload_length];
-    ssize_t bytes_read;
-    ssize_t bytes_to_read;
+u_int16_t get_payload_length(uint8_t first, uint8_t second);
 
-    server_fd = *(int*)server_fd_ptr;
+void parse_response(int type, void *server_fd_ptr, uint16_t payload_length, int response_length);
 
-    bytes_to_read = payload_length + RESPONSE_HEADER_SIZE;
+// void handle_diagnostics();
 
-    bytes_read = read(server_fd, &response, (size_t)bytes_to_read);
+// void handle_();
 
-    if(bytes_read < 4)
-    {
-        printf("Error reading response payload.\n");
-        return;
-    }
+// void handle_manager_response();
 
-    for ()
-}
+// void get_data_type();
+//
+// void get_data_length();
 
 void *handle_server_response(void *arg)
 {
-    int                 server_fd;
-    ssize_t             bytes_recieved;
-    unsigned char       server_response_header[RESPONSE_HEADER_SIZE];
-    unsigned char       server_response_type;
-    unsigned char       server_response_version;
-    unsigned char       server_response_payload_length;
+    int           server_fd;
+    ssize_t       bytes_recieved;
+    unsigned char response_header[RESPONSE_HEADER_SIZE];
+    unsigned char response_type;
+    uint8_t       response_version;
+    uint16_t      response_payload_length;
+    uint8_t       length_first_byte;
+    uint8_t       length_second_byte;
+    int           response_length;
 
     server_fd = *(int *)arg;
     free(arg);
 
-    bytes_recieved = read(server_fd, server_response_header, RESPONSE_HEADER_SIZE);
+    bytes_recieved = read(server_fd, response_header, RESPONSE_HEADER_SIZE);
 
     if(bytes_recieved < 4)
     {
@@ -60,21 +55,53 @@ void *handle_server_response(void *arg)
         return NULL;
     }
 
-    server_response_version = server_response_header[RESPONSE_VERSION_INDEX];
+    response_version = response_header[RESPONSE_VERSION_INDEX];
 
-    if (server_response_version != REQUIRED_PROTOCOL_VERSION)
+    if(response_version != REQUIRED_PROTOCOL_VERSION)
     {
         printf("Server response version not supported\n");
         close(server_fd);
         return NULL;
     }
 
-    server_response_type = server_response_header[RESPONSE_TYPE_INDEX];
+    response_type = response_header[RESPONSE_TYPE_INDEX];
 
-    server_response_payload_length = server_response_header[RESPONSE_PAYLOAD_LENGTH_INDEX];
+    length_first_byte  = response_header[RESPONSE_PAYLOAD_LENGTH_INDEX_1];
+    length_second_byte = response_header[RESPONSE_PAYLOAD_LENGTH_INDEX_2];
 
-    parse_response(server_response_type, &server_fd, server_response_payload_length);
+    response_payload_length = get_payload_length(length_first_byte, length_second_byte);
+
+    response_length = response_payload_length + RESPONSE_HEADER_SIZE;
+
+    parse_response(response_type, &server_fd, response_payload_length, response_length);
 
     close(server_fd);
     return NULL;
+}
+
+u_int16_t get_payload_length(u_int8_t first, u_int8_t second)
+{
+    u_int16_t length;
+    length = (u_int16_t)((first << BIT_SHIFT_BIG_ENDIAN) | second);
+    return length;
+}
+
+void parse_response(int type, int server_fd, u_int16_t payload_length, int response_length)
+{
+    unsigned char response[RESPONSE_HEADER_SIZE];
+    // unsigned char payload[payload_length];
+    ssize_t bytes_read;
+    ssize_t bytes_to_read;
+
+    bytes_to_read = payload_length + RESPONSE_HEADER_SIZE;
+
+    bytes_read = read(server_fd, &response, (size_t)bytes_to_read);
+
+    if(bytes_read < 4)
+    {
+        printf("Error reading response payload %d.\n", response_length);
+        return;
+    }
+
+    printf("Received response payload type %d:\n", type);
 }
