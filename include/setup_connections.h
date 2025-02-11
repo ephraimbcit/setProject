a35930310bb16cc3cad4a037138548505ef1ecd5
Ephraim Hsu <ehsu18@my.bcit.ca>
2025-02-11 12:08:13 -0800

    SAVE work

---
+++ include/setup_connections.h
@@ -0,0 +1,8 @@
+//
+// Created by ephraim on 2/11/25.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int setup_connections(int *fd, int type);
---
+++ src/setup_connections.c
@@ -0,0 +1,18 @@
+//
+// Created by ephraim on 2/11/25.
+//
+
+#include <pthread.h>
+
+#define TYPE_STARTER 0
+#define TYPE_SERVER 1
+#define TYPE_CLIENT 2
+
+#include "../include/setup_connections.h"
+
+int setup_connections(int *fd, int type)
+{
+    pthread_t connection_thread;
+
+    return EXIT_SUCCESS;
+}
